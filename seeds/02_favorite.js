840a519564e8819dae061eaca09cd0dc76c1a4f1
amberjohnsonsmile <amber.johnson.smile@gmail.com>
2018-01-31 11:54:05 -0700

    Restart ID sequence

--- seeds/02_favorite.js
+++ seeds/02_favorite.js
@@ -2,9 +2,12 @@ exports.seed = function(knex, Promise) {
   return knex('favorite').del()
     .then(() => {
       return knex('favorite').insert([
-        {quote_id: 3, name: 'Amber', location: 'Denver, USA'},
-        {quote_id: 1, name: 'Fred', location: 'London, UK'},
-        {quote_id: 2, name: 'Voldemort', location: 'Tirana, Albania'}
+        {id: 1, quote_id: 3, name: 'Amber', location: 'Denver, USA'},
+        {id: 2, quote_id: 1, name: 'Fred', location: 'London, UK'},
+        {id: 3, quote_id: 2, name: 'Voldemort', location: 'Tirana, Albania'}
       ]);
+    })
+    .then(() => {
+      return knex.raw('ALTER SEQUENCE favorite_id_seq RESTART WITH 4');
     });
 };
