613f30cfdde1189e5cb6bd5603850e22915e76f4
amberjohnsonsmile <amber.johnson.smile@gmail.com>
2018-02-04 18:00:54 -0700

    Add quotes from Prisoner of Azkaban

--- seeds/01_quote.js
+++ seeds/01_quote.js
@@ -184,8 +184,161 @@ exports.seed = function(knex, Promise) {
         {
           id: 22,
           quote:
-            "",
+            "Harry’s insides did a horrible somersault.",
           source: 'Harry Potter and the Chamber of Secrets',
+          image:
+            'https://images.unsplash.com/photo-1512408937761-ab688c0008bc',
+          isReal: true
+        },
+        {
+          id: 23,
+          quote:
+            "Harry’s stomach gave a funny jolt.",
+          source: 'Harry Potter and the Prisoner of Azkaban',
+          image:
+            'https://images.unsplash.com/photo-1501602883426-b7e5237587b4',
+          isReal: true
+        },
+        {
+          id: 24,
+          quote:
+            "A feeling of great gloom in his stomach, Harry pulled the door open.",
+          source: 'Harry Potter and the Prisoner of Azkaban',
+          image:
+            'https://images.unsplash.com/photo-1494791368093-85217fbbf8de',
+          isReal: true
+        },
+        {
+          id: 25,
+          quote:
+            "She jerked her head at Harry, who felt his stomach clench.",
+          source: 'Harry Potter and the Prisoner of Azkaban',
+          image:
+            'https://images.unsplash.com/photo-1475868530036-7e1f42b9c91c',
+          isReal: true
+        },
+        {
+          id: 26,
+          quote:
+            "His stomach churned as he fell back to wondering what was going to happen to him, and whether the Dursleys had managed to get Aunt Marge off the ceiling yet.",
+          source: 'Harry Potter and the Prisoner of Azkaban',
+          image:
+            'https://images.unsplash.com/photo-1487615215796-62477c922fa5',
+          isReal: true
+        },
+        {
+          id: 27,
+          quote:
+            "Harry looked up at the owner of the hand on his shoulder and felt a bucketful of ice cascade into his stomach — he had walked right into Cornelius Fudge, the Minister of Magic himself.",
+          source: 'Harry Potter and the Prisoner of Azkaban',
+          image:
+            'https://images.unsplash.com/photo-1511699969499-46229c53fc12',
+          isReal: true
+        },
+        {
+          id: 28,
+          quote:
+            "Harry’s eyes darted downward, and what he saw made his stomach contract.",
+          source: 'Harry Potter and the Prisoner of Azkaban',
+          image:
+            'https://images.unsplash.com/photo-1470162415307-7ed32bad0a5a',
+          isReal: true
+        },
+        {
+          id: 29,
+          quote:
+            "Harry’s stomach lurched.",
+          source: 'Harry Potter and the Prisoner of Azkaban',
+          image:
+            'https://images.unsplash.com/photo-1502035458144-454aa46b8ee0',
+          isReal: true
+        },
+        {
+          id: 30,
+          quote:
+            "“Right,” he said, trying to recall as exactly as possible the wonderful, soaring sensation of his stomach.",
+          source: 'Harry Potter and the Prisoner of Azkaban',
+          image:
+            'https://images.unsplash.com/photo-1422132940975-a71d3ffef351',
+          isReal: true
+        },
+        {
+          id: 31,
+          quote:
+            "Harry’s stomach turned over — Dumbledore would know exactly what had happened, if Malfoy said anything.",
+          source: 'Harry Potter and the Prisoner of Azkaban',
+          image:
+            'https://images.unsplash.com/photo-1512685076844-4161e436c4f5',
+          isReal: true
+        },
+        {
+          id: 32,
+          quote:
+            "Harry was sitting with Ron and Hermione, removed from the center of things, trying not to think about the next day, because every time he did, he had the horrible sensation that something very large was fighting to get out of his stomach.",
+          source: 'Harry Potter and the Prisoner of Azkaban',
+          image:
+            'https://images.unsplash.com/photo-1508454868649-abc39873d8bf',
+          isReal: true
+        },
+        {
+          id: 33,
+          quote:
+            "“Yeah...,” said Harry, his stomach writhing.",
+          source: 'Harry Potter and the Prisoner of Azkaban',
+          image:
+            'https://images.unsplash.com/photo-1501645285-fe34b236a8ab',
+          isReal: true
+        },
+        {
+          id: 34,
+          quote:
+            "Harry felt as though the bottom had dropped out of his stomach.",
+          source: 'Harry Potter and the Prisoner of Azkaban',
+          image:
+            'https://images.unsplash.com/photo-1512511708753-3150cd2ec8ee',
+          isReal: true
+        },
+        {
+          id: 35,
+          quote:
+            "Some sort of explosion took place in the pit of Harry’s stomach.",
+          source: 'Harry Potter and the Prisoner of Azkaban',
+          image:
+            'https://images.unsplash.com/photo-1514854148010-54ed5354ca86',
+          isReal: true
+        },
+        {
+          id: 36,
+          quote:
+            "It was as though freezing water were rising in his chest, cutting at his insides.",
+          source: 'Harry Potter and the Prisoner of Azkaban',
+          image:
+            'https://images.unsplash.com/photo-1488227402652-00175718582c',
+          isReal: true
+        },
+        {
+          id: 37,
+          quote:
+            "Harry’s insides lurched.",
+          source: 'Harry Potter and the Prisoner of Azkaban',
+          image:
+            'https://images.unsplash.com/photo-1492232093388-4d9d123d362d',
+          isReal: true
+        },
+        {
+          id: 38,
+          quote:
+            "He spun around, the familiar, icy cold penetrating his insides, fog starting to obscure his vision; more were appearing out of the darkness on every side; they were encircling them....",
+          source: 'Harry Potter and the Prisoner of Azkaban',
+          image:
+            'https://images.unsplash.com/photo-1504252060324-1c76e2e09939',
+          isReal: true
+        },
+        {
+          id: 39,
+          quote:
+            "",
+          source: 'Harry Potter and the Prisoner of Azkaban',
           image:
             '',
           isReal: true
